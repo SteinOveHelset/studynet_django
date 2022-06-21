7e8045e1dae721f2b95bfb03bda80996f17237e5
Code With Stein <codewithstein@gmail.com>
2022-06-21 19:31:57 +0200

    part 16

--- course/models.py
+++ course/models.py
@@ -15,6 +15,16 @@ class Category(models.Model):
         return self.title
 
 class Course(models.Model):
+    DRAFT = 'draft'
+    IN_REVIEW = 'in_review'
+    PUBLISHED = 'published'
+
+    STATUS_CHOICES = (
+        (DRAFT, 'Draft'),
+        (IN_REVIEW, 'In review'),
+        (PUBLISHED, 'Published')
+    )
+
     categories = models.ManyToManyField(Category)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
@@ -23,6 +33,7 @@ class Course(models.Model):
     created_at = models.DateField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name='courses', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='uploads', blank=True, null=True)
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=DRAFT)
 
     class Meta:
         ordering = ('-created_at',)
--- course/views.py
+++ course/views.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.utils.text import slugify
@@ -11,13 +13,19 @@ from .serializers import CourseListSerializer, CourseDetailSerializer, LessonLis
 
 @api_view(['POST'])
 def create_course(request):
+    status = request.data.get('status')
+
     print(request.data)
+
+    if status == 'published':
+        status = 'draft'
 
     course = Course.objects.create(
         title=request.data.get('title'),
-        slug=slugify(request.data.get('title')),
+        slug='%s-%s' % (slugify(request.data.get('title')), randint(1000, 10000)),
         short_description=request.data.get('short_description'),
         long_description=request.data.get('long_description'),
+        status=status,
         created_by=request.user
     )
 
@@ -26,9 +34,19 @@ def create_course(request):
     
     course.save()
 
-    print(course)
+    # Lessons
 
-    return Response({'yo': 'yo'})
+    for lesson in request.data.get('lessons'):
+        tmp_lesson = Lesson.objects.create(
+            course=course,
+            title=lesson.get('title'),
+            slug=slugify(lesson.get('title')),
+            short_description=lesson.get('short_description'),
+            long_description=lesson.get('long_description'),
+            status=Lesson.DRAFT
+        )
+
+    return Response({'course_id': course.id})
 
 @api_view(['GET'])
 def get_quiz(request, course_slug, lesson_slug):
@@ -50,7 +68,7 @@ def get_categories(request):
 @permission_classes([])
 def get_courses(request):
     category_id = request.GET.get('category_id', '')
-    courses = Course.objects.all()
+    courses = Course.objects.filter(status=Course.PUBLISHED)
 
     if category_id:
         courses = courses.filter(categories__in=[int(category_id)])
@@ -62,13 +80,13 @@ def get_courses(request):
 @authentication_classes([])
 @permission_classes([])
 def get_frontpage_courses(request):
-    courses = Course.objects.all()[0:4]
+    courses = Course.objects.filter(status=Course.PUBLISHED)[0:4]
     serializer = CourseListSerializer(courses, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def get_course(request, slug):
-    course = Course.objects.get(slug=slug)
+    course = Course.objects.filter(status=Course.PUBLISHED).get(slug=slug)
     course_serializer = CourseDetailSerializer(course)
     lesson_serializer = LessonListSerializer(course.lessons.all(), many=True)
 
@@ -103,7 +121,7 @@ def add_comment(request, course_slug, lesson_slug):
 @api_view(['GET'])
 def get_author_courses(request, user_id):
     user = User.objects.get(pk=user_id)
-    courses = user.courses.all()
+    courses = user.courses.filter(status=Course.PUBLISHED)
 
     user_serializer = UserSerializer(user, many=False)
     courses_serializer = CourseListSerializer(courses, many=True)
